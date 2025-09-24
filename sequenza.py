6675601107140ae395f2e4c7bc20e7fa4eca2800
Chris <chris.dv04@gmail.com>
2025-09-24 11:48:10 +0200

    piccola correzione

--- sequenza.py
+++ sequenza.py
@@ -1,7 +1,7 @@
 from utils import *
 
 if __name__=="__main__":
-    codifica={"zero":0,"uno":1,"due":2,"tre":3,"quattro":4,"cinque":5,"sei":6,"sette":7,"otto":8,"nove":9}
+    cifre={"zero":0,"uno":1,"due":2,"tre":3,"quattro":4,"cinque":5,"sei":6,"sette":7,"otto":8,"nove":9}
     somma=0
     filename=input("Nome del file di input: ")
     f=openFileForReading(filename)
@@ -10,11 +10,12 @@ if __name__=="__main__":
             numeri=riga.strip().split()
             risultato=""
             for n in numeri:
-                if n in codifica:
-                    risultato+=str(codifica[n])
+                if n in cifre:
+                    risultato+=str(cifre[n])
                 elif n=="stop":
                     print(risultato)
-                    somma+=int(risultato)
+                    if risultato!="":
+                        somma+=int(risultato)
                     risultato=""
                 else:
                     print("Parola non valida: "+n)
